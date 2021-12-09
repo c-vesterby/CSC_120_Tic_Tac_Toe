858029f5489b83d9e17295576773267ef9ef19e2
c-vesterby <christinevesterby@gmail.com>
2021-12-09 12:13:38 -0500

    board

--- board.py
+++ board.py
@@ -1,32 +1,9 @@
 board = [["-","-","-"],
-         ["-","-","-"],
-         ["-","-","-"],]
+        ["-","-","-"],
+        ["-","-","-"],]
+        
+def print_board():
+    for row in board:
+            print(row)
 
-for row in board:
-    print(row)
-
-
-
-#def printBoard(board):
-#    for i in range(len(board)):
-#        row = ''
-#        for j in range (len(board[i])):
-#            row = row + "[" + board[i][j] + "]"
-#       print(row)
-
-# # list.append(l1)
-# # list.append(l2)
-# # list.append(l3)
-
-# # print(list)
-
-# # for i in range(3):
-#  #   list.append([])
-#    # for j in range(3):
-#       #  list[i].append(j)
-
-
-# for i in range(3):
-#     row = []
-#     for j in range(3):
-#         row.append("_")
+print_board()
