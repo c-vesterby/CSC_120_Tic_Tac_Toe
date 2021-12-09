0a9d40e488a3b75e63bd6132c716f390fa8540d1
c-vesterby <christinevesterby@gmail.com>
2021-12-09 14:18:59 -0500

    Trying to determine a winner

--- board.py
+++ board.py
@@ -1,12 +1,5 @@
 import itertools
 
-# player_id = itertools.cycle([1,2])
-# new_board = board.replace(player_input)
-# rows = [0,1,2]
-# colums = [0,1,2]
-
-
-
 def new_board():
     return [
         ["-","-","-"],
@@ -18,26 +11,20 @@ def print_board(board):
     for row in board:
             print(row)
 
-def player_input(board, row, column, player_id):
-    if player_id == 1:
-        add_mark(board, row, column, "X") 
-    else: 
-        add_mark(board, row, column, "O")
-
 def add_mark(board, row, column, mark):
     if check_size(board, row):
         if check_size(board[row], column):
             if not check_mark(board, row, column):
                 board[row][column] = mark
             else: 
-                print("This location is already taken.")
+                return "This location is already taken."
         else:
-            print("invalid column size")
+            return "invalid column size"
     else:
-        print("invalid row size")
+        return "invalid row size"
 
 def check_size(line, value):
-    if value > 0 and value < len(line):
+    if value >= 0 and value < len(line):
         return True
     
     return False
@@ -45,15 +32,68 @@ def check_size(line, value):
 def check_mark(board, row, column):
     return board[row][column] != "-"
 
+def mark(player_id):
+    if player_id == 1:
+        return "X"
+    else:
+        return "O"
+
+def check_win(board):
+    for row in board:
+        current_mark ="" 
+        count = 0
+        for mark in row:
+            if mark != "-":
+                if count == 0:
+                    count = count + 1
+                    current_mark = mark
+
+            
+
+    # for col in range(len(board)):
+    #     check = []
+
+    #     for row in board:
+    #         check.append(row[col])
+
+    #     if row.count(row[0]) == len(row) and row [0] != ("-"):
+    #             print("Winner!")
+    #             return True
+
+    # diag = []
+    # for col, row in enumerate(reversed(range(len(board)))):
+    #     diag.append(board[row][col])
+    # if diag.count(diag[0]) == len(diag) and diag [0] != 0:
+    #             print("Winner!")
+    #             return True
+
+    # diag = []
+    # for ix in range(len(board)):
+    #     diag.append(board[ix][ix])
+    # if diag.count(diag[0]) == len(diag) and diag [0] != 0:
+    #             print("Winner!")
+    #             return True
+    # return False    
+
 def tic_tac_toe():
     board = new_board()
     print("Welcome to Tic Tac Toe")
     print_board(board)
-    print("Player 1 place your X")
-    row = int(input("Enter row: "))
-    column = int(input("Enter column: "))
-    player_input(board, row, column, 1)
-    print_board(board)
+
+    for player_id in itertools.cycle([1,2]):
+        print("Player " + str(player_id) + " place your " + mark(player_id))
+        
+        #Need loop to run at least once.
+        error = ""
+        while error is not None:
+            if len(error) > 0:
+                print("Invalid location, " + error)
+            #TODO Enter is invalid when value is not a integer
+            row = int(input("Enter row: "))
+            column = int(input("Enter column: "))
+            error = add_mark(board, row, column, mark(player_id))
+        print_board(board)
+        check_win(board)
 
 
 tic_tac_toe()     
